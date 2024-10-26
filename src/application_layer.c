f56427bdab82f0efc74346ab9aa8a193f2d4a3ae
coltginger <gnbarroso@gmail.com>
2024-10-26 19:55:14 +0100

    Update application_layer.c
    update incompleto que tive de dar push pq fui jantar

--- src/application_layer.c
+++ src/application_layer.c
@@ -7,5 +7,21 @@
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
-    // TODO
+    LinkLayer connectionParameters;
+    LinkLayerRole linkLayerRole;
+    linkLayerRole = (strcmp(role, "tx") == 0) ? LlTx : LlRx;
+
+    strcpy(connectionParameters.serialPort, serialPort);
+    connectionParameters.role = linkLayerRole;
+    connectionParameters.baudRate = baudRate;
+    connectionParameters.nRetransmissions = nTries;
+    connectionParameters.timeout = timeout;
+
+    if (llopen(connectionParameters) < 0)
+    {
+        printf("Failed to establish connection\n");
+        exit(-1);
+    }
+
+
 }
